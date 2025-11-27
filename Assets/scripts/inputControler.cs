cd7789e9670641561942b7870f4ee89e9fc19b15
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-27 11:58:32 -0400

    had to purge old code a second time cause git bein a git

--- Assets/scripts/inputControler.cs
+++ Assets/scripts/inputControler.cs
@@ -4,21 +4,32 @@ using UnityEngine.InputSystem.XR;
 public class inputcontroler : MonoBehaviour
 {
 
+   [SerializeField] playercontroler CharacterController;
 
-    public playercontroler CharacterController;
-
-;
+    InputAction moveAction;
+    InputAction lookAction;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
+        lookAction = InputSystem.actions.FindAction("Look");
+
+        Cursor.visible = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector2 movementVector = moveAction.ReadValue<Vector2>();
+        CharacterController.Move(movementVector);
+
+        Vector2 lookVector = lookAction.ReadValue<Vector2>();
+        CharacterController.Rotate(movementVector);
+
+
+
     }
 }
